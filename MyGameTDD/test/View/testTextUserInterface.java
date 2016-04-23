5ebc59320bd2c6ecee12d4e28174c73da739a893
Hampus Carlbom <haca89@gmail.com>
2016-04-23 13:12:16 +0200

    TextUserInterface created and under dev.

---
+++ MyGameTDD/test/View/testTextUserInterface.java
@@ -0,0 +1,26 @@
+package View;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testTextUserInterface {
+
+	TextUserInterface ui;
+	
+	@Before
+	public void setUp() {
+		ui = new TextUserInterface();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	private void testCreateGUI() {
+
+	}
+}
