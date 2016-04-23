ab1e48e8b76db32546c821825b522b63a69620d3
Hampus Carlbom <haca89@gmail.com>
2016-04-23 16:30:50 +0200

    Few tests done on Sysot.

--- MyGameTDD/src/View/TextUserInterface.java
+++ MyGameTDD/src/View/TextUserInterface.java
@@ -2,4 +2,8 @@ package View;
 
 public class TextUserInterface {
 
+	public void print(String string) {
+		System.out.print(string);
+	}
+
 }
--- MyGameTDD/test/View/testSystemOut.java
+++ MyGameTDD/test/View/testSystemOut.java
@@ -9,21 +9,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class testTextUserInterface {
-
-	
-	
-	TextUserInterface ui;
-	//Scanner sc = new Scanner(System.console().readLine());
+public class testSystemOut {
 	
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	
-	@Before
-	public void setUp() {
-		ui = new TextUserInterface();
-	}
-	
+
 	@Before
 	public void setUpStreams() {
 	    System.setOut(new PrintStream(outContent));
@@ -37,31 +27,9 @@ public class testTextUserInterface {
 	}
 	
 	@Test
-	public void testPrintText() {
-		String testMessage = "Hello World!";
-		ui.print(testMessage);
-		assertEquals(testMessage, outContent.toString());
-	}
-	
-	@Test
-	public void testPrintTextWithMatches() {
-		String testMessage = "Hello World!";
-		ui.print(testMessage);
-		assertTrue(testMessage.matches(outContent.toString()));
-	}
-	
-	
-	
-	@Test
 	public void out() {
 	    System.out.print("hello");
 	    assertEquals("hello", outContent.toString());
-	}
-	
-	@Test
-	public void outWithMatches() {
-	    System.out.print("hello");
-	    assertTrue("hello".matches(outContent.toString()));
 	}
 
 	@Test
@@ -69,5 +37,4 @@ public class testTextUserInterface {
 	    System.err.print("hello again");
 	    assertEquals("hello again", errContent.toString());
 	}
-
 }
